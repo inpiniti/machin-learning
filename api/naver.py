75469061a96de1c0fb9261425a841e32dcdc620d
inpiniti <younginpiniti@gmail.com>
2022-11-19 13:48:06 +0900

    FEAT - naver api 글거 오는 부분 구현 Author : inpiniti Date : 2022-11-19
      * 네이버 검색
      * 네이버 토론
      * 네이버 뉴스
      * 다음의 경우 403 에러가 뜸 (postman에서는 정상동작하는데도 불구하고)

--- api/naver.py
+++ api/naver.py
@@ -1,31 +1,53 @@
 import requests
 import json
 
-# 네이버 검색
-def send_api(path, method):
-    API_HOST = "http://www.example.com"
+def naverApi(path):
+    API_HOST = "https://api.stock.naver.com"
     url = API_HOST + path
-    headers = {'Content-Type': 'application/json', 'charset': 'UTF-8', 'Accept': '*/*'}
-    body = {
-        "key1": "value1",
-        "key2": "value2"
-    }
+    return sendApi(url)
 
+def daumApi(path):
+    API_HOST = "https://finance.daum.net"
+    url = API_HOST + path
+    sendApi(url, {
+        "referer": "https://m.finance.daum.net/",
+        "Accept": "*/*",
+        "Host": "finance.daum.net",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive"
+    })
+
+def sendApi(url, headers = {
+    'Content-Type': 'application/json',
+    'charset': 'UTF-8',
+    'Accept': '*/*',
+}):
     try:
-        if method == 'GET':
-            response = requests.get(url, headers=headers)
-        elif method == 'POST':
-            response = requests.post(url, headers=headers, data=json.dumps(body, ensure_ascii=False, indent="\t"))
-        print("response status %r" % response.status_code)
-        print("response text %r" % response.text)
+        response = requests.get(url, headers=headers)
+
+        list = []
+        for i in response.json()["datas"]:
+            list.append(i['stockName'])
+        return list
     except Exception as ex:
-        print(ex)
-ㅣ
+        return ex
+
+# 네이버 검색
+def stock():
+    return naverApi("/ranking/stock/local/total")
+
 # 네이버 토론
+def discussion():
+    return naverApi("/ranking/discussion/local/stock_item")
+
 # 네이버 뉴스
+def news():
+    return naverApi("/ranking/news/local/stock_local")
+
 # 다음 인기
+def popular():
+    return daumApi("/content/debate/popular?type=STOCK&limit=10")
+
 # 다음 조회 급등
-
-
-# 호출 예시
-send_api("/test", "POST")
+def search():
+    return daumApi("/api/search/ranks?limit=10")
