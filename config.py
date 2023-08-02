46817002d9442f2dbed1ac3d0f021fc6aa07102d
inpiniti <younginpiniti@gmail.com>
2023-08-02 16:59:27 +0900

    pysql config 추가

--- config.py
+++ config.py
@@ -2,3 +2,10 @@ allPredictResult = None
 stockPredictResult = None
 discussionPredictResult = None
 newsPredictResult = None
+pysql = {
+    'host': '110.46.192.54',
+    'port': '3306',
+    'db': 'python-inpiniti',
+    'id': 'root',
+    'pw': '!Wjd53850',
+}
